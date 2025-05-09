7a55bf633dc9813a22c257b61162b7319c78badf
soccer1312 <leiss2b96@gmail.com>
2025-05-09 16:41:31 -0700

    Fix: Restructure Netlify functions for Python deployment

--- netlify/functions/index.js
+++
@@ -1,6 +0,0 @@
-// This file helps Netlify recognize the functions directory
-// The actual functions are in the api and proxy-image subdirectories
-module.exports = {
-  api: require('./api/index.py'),
-  'proxy-image': require('./proxy-image/index.py')
-}; 
